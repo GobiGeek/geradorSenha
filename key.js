542a808a8354b3d623a3b8006442b5a414b83594
GobiGeek <leonardofbrambini@gmail.com>
2026-01-30 11:52:50 -0300

    testando alteração

--- key.js
+++ key.js
@@ -1,13 +1,15 @@
 function gerarSenha(comprimento) {
   let caracteres =
-    "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz123456789!#$%&*()";
-  let senha = "";
+    'ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz123456789!#$%&*()'
+  let senha = ''
+
+  const teste = 0
 
   for (let i = 0; i < comprimento; i++) {
-    let randomIndex = Math.floor(Math.random() * caracteres.length);
-    senha += caracteres[randomIndex];
+    let randomIndex = Math.floor(Math.random() * caracteres.length)
+    senha += caracteres[randomIndex]
   }
-  return senha;
+  return senha
 }
 
-console.log(gerarSenha(6));
+console.log(gerarSenha(6))
